7ba579f2f103441ada4e0a5045862780a7171491
tiptoemosquito <clhoward33@icloud.com>
2020-10-26 18:29:32 -0700

    migrate

--- db/migrate/20201027005237_create_art_supplies_table.rb
+++ db/migrate/20201027005237_create_art_supplies_table.rb
@@ -1,4 +1,9 @@
 class CreateArtSuppliesTable < ActiveRecord::Migration
   def change
+    create_table :art_supplies do |t|
+      t.string :medium
+      t.string :tools
+      t.string :categories_id #integer?
+    end
   end
 end
