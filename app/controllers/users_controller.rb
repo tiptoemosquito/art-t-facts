e2381985366853d37ae5cf3bda6f545172a474f2
tiptoemosquito <clhoward33@icloud.com>
2020-10-27 09:55:22 -0700

    views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,45 @@
 class UsersController < ApplicationController
+
+    get '/' do 
+        erb :
+    end
+
+    get 'signup' do
+        if !logged_in?
+            erb :'user/create_user', locals: {mesage: "New? Sign Up now! Then sign in once completed!"}
+        else 
+            redirect to '/'
+        end
+    end
+
+    post 'signup' do 
+        if params[:username] == "" || params[:email] == || params[:password] == ""
+            redirect to '/signup'
+        else 
+            @user = User.new(:username => params[:username], :email => params[:email], :password => params[:passowrd])
+            @user.save
+            session[:user_id] = @user.id 
+            redirect to '/'
+        end
+    end
+
+    get 'login' do
+        if !logged_in?
+            erb :'user/login'
+        else
+            redirect to '/'
+        end 
+    end
+
+    post '/login' do
+        user = User.find_by(:username => params[:username])
+        if user && user.autheniticate(params[:password])
+            session[:user_id] = user.user_id
+            redirect to '/'
+        else 
+            redirect to '/signup'
+        end
+    end
+
+
 end
