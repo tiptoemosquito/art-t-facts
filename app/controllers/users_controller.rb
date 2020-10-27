79fd27f3acc277551c05478a1dec8e2ad87ebb1e
tiptoemosquito <clhoward33@icloud.com>
2020-10-27 10:56:41 -0700

    app

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,26 @@
 class UsersController < ApplicationController
 
-    # get '/' do 
+    # get '/users/:slug' do
+    #     @user = User.find_by_slug(params[:slug]) 
     #     erb :'/users/show'
     # end
 
     # get '/signup' do
     #     if !logged_in?
-    #         erb :'users/create_user', locals: {mesage: "New? Sign Up now! Then sign in once completed!"}
+    #         erb :'users/create_user', locals: {message: "New? Sign Up now! Then sign in once completed!"}
     #     else 
-    #         redirect to '/'
+    #         redirect to 'users/singup'
     #     end
     # end
 
     # post '/signup' do 
     #     if params[:username] == "" || params[:email] == || params[:password] == ""
-    #         redirect to '/signup'
+    #         redirect to 'users/signup'
     #     else 
     #         @user = User.new(:username => params[:username], :email => params[:email], :password => params[:passowrd])
     #         @user.save
     #         session[:user_id] = @user.id 
-    #         redirect to '/'
+    #         redirect to 'users/signup'
     #     end
     # end
 
@@ -27,7 +28,7 @@ class UsersController < ApplicationController
     #     if !logged_in?
     #         erb :'users/login'
     #     else
-    #         redirect to '/'
+    #         redirect to 'users/login'
     #     end 
     # end
 
@@ -35,9 +36,8 @@ class UsersController < ApplicationController
     #     user = User.find_by(:username => params[:username])
     #     if user && user.autheniticate(params[:password])
     #         session[:user_id] = user.user_id
-    #         redirect to '/'
     #     else 
-    #         redirect to '/signup'
+    #         redirect to 'users/signup'
     #     end
     # end
 
